04d4147910ce3447eabfc291e1b594f306475f90
TravelFiend <mikegrace274@gmail.com>
2019-12-08 12:39:28 -0800

    second practice exercise in place

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+// http.createServer((req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.write("Hello World");
+//     res.end();
+// }).listen(6789);
+
+const onRequest = (req, res) => {
+    console.log('Request recieved');
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello World");
+    res.write("GobblyGook");
     res.end();
-}).listen(6789);
+};
+
+http.createServer(onRequest).listen(6789);
+
+console.log('Server has started');
